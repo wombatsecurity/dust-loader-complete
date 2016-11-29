641732fe19c4dd8ff96f799c6e73f0e686fab043
Arnout Evers <arnout.evers@gmail.com>
2016-11-29 17:35:59 +0100

    Added config option for disabling whitespace trimming (#14)
    * Added option to prevent Dust from trimming whitespace before compiling
    
    * dust.config.whitespace should be set to true to disable whitespace trimming
    
    * Removed redundant newlines
    
    * Added unit tests for verifying behavior of preserveWhitespace option
    
    * Tweaked test case descriptions

---
+++ test/whitespace.test.js
@@ -0,0 +1,46 @@
+var templateWhitespacePreserved = require( "!!preserve-whitespace!whitespace" );
+var templateWhitespaceNotPreserved = require( "whitespace" );
+
+describe( "option preserveWhitespace", function( ) {
+
+    it( "should cause whitespace chars to have been removed in output of templateFn, in case option is disabled or not configured (default)", function( done ) {
+
+        var output;
+        var matcher = /\n\s/g;
+
+        templateWhitespaceNotPreserved( {}, function( err, out ) {
+
+          if ( err )
+            return done( err );
+
+          output = out;
+
+        });
+
+        expect( output.match( matcher ) ).to.be.null;
+
+        done();
+
+    });
+
+    it( "should cause whitespace chars to have been preserved in in output of templateFn, in case option is enabled", function( done ) {
+
+        var output;
+        var matcher = /\n\s/g;
+
+        templateWhitespacePreserved( {}, function( err, out ) {
+
+          if ( err )
+            return done( err );
+
+          output = out;
+
+        });
+
+        expect( output.match( matcher ) ).to.not.be.empty;
+
+        done();
+
+    });
+
+});
