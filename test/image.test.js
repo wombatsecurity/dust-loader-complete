a3f7e2137f970c8a6504d1bf427759fd946cc7ba
Jason Brubaker <jrrbrubaker@gmail.com>
2018-08-02 07:56:19 -0400

    Support image exclusion regex (#28)
    * Add an option to exclude some images from resolution
    
    * 4.1.2

--- test/image.test.js
+++ test/image.test.js
@@ -2,18 +2,26 @@ var path = require('path');
 var dust = require('dustjs');
 var template = require('image');
 
-describe("<img> tags", function () {
-    it('have their "src" attributes converted to the proper relative path', function (done) {
+describe("an <img> tag", function () {
+    it('has its "src" attributes converted to the proper relative path', function (done) {
         dust.render(template, {}, function (err, output) {
             // look for first tag
             expect(output.indexOf('<img id="first" width="250px" src="/assets/images/dolphin.jpg" style="display:block" />')).to.be.greaterThan(-1);
+            done(err);
+        });
+    });
 
-            // look for second tag
+    it('supports multiple <img> tags per line', function (done) {
+        dust.render(template, {}, function (err, output) {
             expect(output.indexOf('<img src = "/assets/images/dolphin.jpg"/>')).to.be.greaterThan(-1);
+            expect(output.indexOf('<img src = "/assets/images/dolphin.jpg" id="dolphin2"/>')).to.be.greaterThan(-1);
+            done(err);
+        });
+    });
 
-            // look for third tag
-            expect(output.indexOf('<img src = "/assets/images/dolphin.jpg" id="third"/>')).to.be.greaterThan(-1);
-
+    it('does not change a tag that matches the excludeImageRegex', function (done) {
+        dust.render(template, {}, function (err, output) {
+            expect(output.indexOf('<img src="/foo/{skip_this}"/>')).to.equal(-1);
             done(err);
         });
     });
